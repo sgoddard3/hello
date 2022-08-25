87cf0c73cab955e7624a8ff6892d5a6722bccf52
Sgoddard3 <sgoddard3@huskers.unl.edu>
2022-08-25 13:44:04 +0000

    initial commit

---
+++ hello.c
@@ -0,0 +1,11 @@
+/**
+ * Basic Hello World Program
+ * /
+
+ int main(int argc, char **argv) {
+
+     printf("Hello, I'm Sam Goddard!\n");
+     printf("Major is Electrical Engineering!\n");
+
+     return 0;
+ }
